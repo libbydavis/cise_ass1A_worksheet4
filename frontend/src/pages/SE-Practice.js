4ff402b1a3acfb5a7cb4669178e2960d1bbc6e37
libbydavis <libstarnz@gmail.com>
2022-03-31 14:44:22 +1300

    added table and form with dummy data

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Table from "../components/EvidenceTable";
+import articles from "../dummydata/articles";
+import tablecolumns from "../components/tablecolumns";
+import Dropdown from "../components/Dropdown";
+import Styles from "../components/tablestyle";
 
 const SEPractice = () => {
     return (
         <div>
             <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-            <p>There will be a way to select an SE practice from our SEPER repository here</p>
-            <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
+            <Dropdown/>
+            <Styles>
+                <Table
+                data={articles}
+                columns={tablecolumns}/>
+            </Styles>
         </div>
     );
 }
